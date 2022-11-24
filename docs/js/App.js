0aceebee199ebcc4244cd6de39bef858f630d244
Kynan Stewart Hughes <mail@kynan.net.au>
2022-11-25 07:05:42 +1100

    v1.2: Generated and set up for GitHub Pages.

--- docs/js/App.js
+++ docs/js/App.js
@@ -3,7 +3,6 @@ import { h } from "../web_modules/preact.js";
 import Router from "../web_modules/preact-router.js";
 import { html } from "../web_modules/htm/preact.js";
 import { AppProvider } from "./AppContext.js";
-import BubbleSort from "./BubbleSort.js";
 import ThreeBubbleSort from "./three-bubble-sort/ThreeBubbleSort.js";
 // import registerServiceWorker from "./registerServiceWorker.js";
 const finishCounter = {
