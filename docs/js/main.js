04ad02b1a96fc6b7536dd052457522792f8e93b2
Kynan Stewart Hughes <mail@kynan.net.au>
2022-11-28 08:29:55 +1100

    Deploying

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,4 +3,4 @@ import { h, hydrate, render } from "../web_modules/preact.js";
 import App from "./App.js";
 import { html } from "../web_modules/htm/preact.js";
 
-hydrate(html` <${App} /> `, document.getElementById("goodthing"));
+render(html` <${App} /> `, document.getElementById("goodthing"));
